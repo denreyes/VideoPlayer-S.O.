19ebe0b03924822e731f87232e434a38e7be3f31
Densho <denj.rey@gmail.com>
2023-03-10 00:57:36 -0500

    Exoplayer Implementation

--- app/src/main/java/com/silverorange/videoplayer/ui/main/MainActivity.kt
+++ app/src/main/java/com/silverorange/videoplayer/ui/main/MainActivity.kt
@@ -2,14 +2,22 @@ package com.silverorange.videoplayer.ui.main
 
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
+import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.Player
+import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.exoplayer2.source.DefaultMediaSourceFactory
+import com.google.android.exoplayer2.source.MediaSource
 import com.silverorange.videoplayer.R
 import io.noties.markwon.Markwon
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     private lateinit var viewModel: MainViewModel
+    private lateinit var player: SimpleExoPlayer
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,14 +25,64 @@ class MainActivity : AppCompatActivity() {
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
 
         val markwon = Markwon.create(this)
+        // Create a SimpleExoPlayer instance
+        player = SimpleExoPlayer.Builder(this).build()
+        // Bind the player to the player view
+        player_view.player = player
+        player_view.useController = false
+
+        player.addListener(object : com.google.android.exoplayer2.Player.Listener {
+            override fun onIsPlayingChanged(isPlaying: Boolean) {
+                super.onIsPlayingChanged(isPlaying)
+                play_pause_button.setImageResource(if (isPlaying) R.drawable.pause else R.drawable.play)
+            }
+        })
+
+        next_button.setOnClickListener {
+            viewModel.nextVideo()
+        }
+        prev_button.setOnClickListener {
+            viewModel.prevVideo()
+        }
+        play_pause_button.setOnClickListener {
+            if (player.isPlaying) {
+                player.pause()
+            } else {
+                player.playWhenReady = true
+                player.play()
+            }
+        }
 
         // Observe LiveDatas
         viewModel.video.observe(this) {
             video_title.text = it.title;
             video_author.text = it.author.name;
             markwon.setMarkdown(video_description, it.description)
+
+            val mediaSource: MediaSource = DefaultMediaSourceFactory(this)
+                .createMediaSource(MediaItem.fromUri(Uri.parse(it.fullURL)))
+
+            player.setMediaSource(mediaSource);
+            player.prepare();
+            player.playWhenReady = true
+        }
+
+        viewModel.nextVideoExists.observe(this) {
+            next_button.isEnabled = it
+            next_button.setImageResource(if (it) R.drawable.next else R.drawable.next_opaque)
+        }
+
+        viewModel.prevVideoExists.observe(this) {
+            prev_button.isEnabled = it
+            prev_button.setImageResource(if (it) R.drawable.previous else R.drawable.previous_opaque)
         }
 
         viewModel.fetchVideos()
     }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        player.release()
+    }
+
 }
--- app/src/main/java/com/silverorange/videoplayer/ui/main/MainViewModel.kt
+++ app/src/main/java/com/silverorange/videoplayer/ui/main/MainViewModel.kt
@@ -8,9 +8,10 @@ import com.silverorange.videoplayer.api.ApiRequestListener
 import com.silverorange.videoplayer.api.models.Videos
 import com.silverorange.videoplayer.api.videos.VideosApiCall
 import com.silverorange.videoplayer.utils.AppLogger
-import com.silverorange.videoplayer.utils.Constants
 import org.json.JSONArray
 import org.json.JSONException
+import org.json.JSONObject
+import kotlin.collections.ArrayList
 
 class MainViewModel : ViewModel() {
 
@@ -20,6 +21,16 @@ class MainViewModel : ViewModel() {
         value = Videos()
     }
     var video: MutableLiveData<Videos> = _emptyVideo
+
+    private val _nextVideoExists = MutableLiveData<Boolean>().apply {
+        value = false
+    }
+    var nextVideoExists: MutableLiveData<Boolean> = _nextVideoExists
+
+    private val _prevVideoExists = MutableLiveData<Boolean>().apply {
+        value = false
+    }
+    var prevVideoExists: MutableLiveData<Boolean> = _prevVideoExists
     var index = 0
 
     fun fetchVideos() {
@@ -36,15 +47,36 @@ class MainViewModel : ViewModel() {
                         index = 0
                         video.value = videos[index]
                     }
+                    nextVideoExists.value = videos.size > 0
+                    prevVideoExists.value = false
                 } catch (e: JSONException) {
                     onError(e)
                 }
             }
 
             override fun onError(error: Any?) {
-                AppLogger.longInfo(Constants.TAG, "ERROR! = " + error)
+                AppLogger.longInfo("DENSHO", "ERROR! = " + error)
             }
         })
     }
 
+    fun nextVideo() {
+        if(videos.size > 0 && index < videos.size - 1) {
+            index++
+            video.value = videos[index]
+
+            nextVideoExists.value = index < videos.size - 1
+            prevVideoExists.value = index > 0
+        }
+    }
+
+    fun prevVideo() {
+        if(videos.size > 0 && index > 0) {
+            index--
+            video.value = videos[index]
+
+            nextVideoExists.value = index < videos.size - 1
+            prevVideoExists.value = index > 0
+        }
+    }
 }
