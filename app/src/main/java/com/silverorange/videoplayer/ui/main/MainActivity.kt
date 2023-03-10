5f7bd5e0053df8c39b255324bc9404a177e547df
Densho <denj.rey@gmail.com>
2023-03-10 01:40:02 -0500

    Button Animations

--- app/src/main/java/com/silverorange/videoplayer/ui/main/MainActivity.kt
+++ app/src/main/java/com/silverorange/videoplayer/ui/main/MainActivity.kt
@@ -29,22 +29,45 @@ class MainActivity : AppCompatActivity() {
         player = SimpleExoPlayer.Builder(this).build()
         // Bind the player to the player view
         player_view.player = player
-        player_view.useController = false
 
         player.addListener(object : com.google.android.exoplayer2.Player.Listener {
             override fun onIsPlayingChanged(isPlaying: Boolean) {
                 super.onIsPlayingChanged(isPlaying)
                 play_pause_button.setImageResource(if (isPlaying) R.drawable.pause else R.drawable.play)
+                player_view.postDelayed({
+                    //Do a fade animation when hiding and showing buttons
+                    if (isPlaying) {
+                        animateButtonVisibility(false, play_pause_button, next_button, prev_button)
+                    } else {
+                        animateButtonVisibility(true, play_pause_button, next_button, prev_button)
+                    }
+                }, 2000)
             }
         })
 
+        player_view.videoSurfaceView?.setOnClickListener { view ->
+            animateButtonVisibility(true, play_pause_button, next_button, prev_button)
+            player_view.postDelayed({
+                //Do a fade animation when hiding and showing buttons
+                if (player.isPlaying) {
+                    animateButtonVisibility(false, play_pause_button, next_button, prev_button)
+                } else {
+                    animateButtonVisibility(true, 5000, play_pause_button, next_button, prev_button)
+                }
+            }, 5000)
+        }
+
         next_button.setOnClickListener {
+            animateButtonPressed(next_button)
             viewModel.nextVideo()
         }
         prev_button.setOnClickListener {
+            animateButtonPressed(prev_button)
             viewModel.prevVideo()
         }
         play_pause_button.setOnClickListener {
+            animateButtonPressed(play_pause_button)
+
             if (player.isPlaying) {
                 player.pause()
             } else {
@@ -83,6 +106,29 @@ class MainActivity : AppCompatActivity() {
     override fun onDestroy() {
         super.onDestroy()
         player.release()
+    }
+
+    fun animateButtonPressed(vararg views: View) {
+        for (view in views) {
+            view.animate().scaleX(0.5f).scaleY(0.5f).setDuration(100).withEndAction {
+                view.animate().scaleX(1f).scaleY(1f).setDuration(100)
+            }
+        }
+    }
+
+    private fun animateButtonVisibility(visible: Boolean, vararg views: View) {
+        animateButtonVisibility(visible, 500, *views)
+    }
+
+    private fun animateButtonVisibility(visible: Boolean, duration: Long, vararg views: View) {
+        for (view in views) {
+            if (visible)
+                view.visibility =  View.VISIBLE
+            view.animate().alpha(if (visible) 1f else 0f).setDuration(duration).withEndAction {
+                if (!visible)
+                    view.visibility =  View.GONE
+            }
+        }
     }
 
 }
